43f89c7ba501083af77585561f14123c6f6584bf
haripermadi <harinurpermadi@gmail.com>
2018-02-01 20:59:42 +0700

    add hook pass

--- models/login.js
+++ models/login.js
@@ -1,14 +1,17 @@
 'use strict';
+const bcrypt = require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
   var Login = sequelize.define('Login', {
     email: DataTypes.STRING,
     password: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
   });
+  Login.beforeCreate(function(datauser){
+    const saltRounds = 10;
+    const myPlaintextPassword = datauser.password;
+    return bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
+      // Store hash in your password DB.
+      datauser.password=hash
+  });
+  })
   return Login;
 };
